9711a40e7074c519b0990761aae34b5a04830b30
Wellington Sarmento <wwagner@virtual.ufc.br>
2025-06-15 01:46:46 -0300

    Modificacoes para que o sistema funcione tanto com o sqlite quanto postgres

--- config.py
+++ config.py
@@ -1,22 +1,32 @@
+# config.py
 from enum import Enum
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class DatabaseType(str, Enum):
     SQLITE = "sqlite"
     POSTGRES = "postgres"
 
 class Settings(BaseSettings):
-    DATABASE_TYPE: DatabaseType = DatabaseType.POSTGRES
-    SQLITE_PATH: str = "data/terra_data.sqlite"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    DATABASE_TYPE: DatabaseType
+    SQLITE_PATH: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    # defaults seguros
     POSTGRES_HOST: str = "postgres"
     POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str = "terra"
-    POSTGRES_PASSWORD: str = "Asdsee;30"
-    POSTGRES_DB: str = "geodata"
+    POSTGRES_DB: str   = "geodata"
 
     @property
     def postgres_dsn(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return (
+            f"postgresql://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
 
 settings = Settings()
