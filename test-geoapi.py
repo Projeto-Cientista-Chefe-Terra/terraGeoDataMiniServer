d271d83e8d2d721c4bbbebd3a01fa4797ceec3d6
Wellington Sarmento <wwagner@virtual.ufc.br>
2025-08-18 19:41:04 -0300

    preenchimento dos dados dos assentamentos

--- test-geoapi.py
+++ test-geoapi.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://geoapi.idace.ce.gov.br/geoapi/pessoa/municipio/IRAUCUBA?pagina=0&tamanho=10000&ordenarPor=proprietario" #QUIXADA
+url = "http://geoapi.idace.ce.gov.br/geoapi/pessoa/municipio/VICOSA%20DO%20CEARA?pagina=0&tamanho=1000&ordenarPor=proprietario" #QUIXADA
 
 payload = {}
 headers = {
@@ -10,5 +10,8 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, data=payload)
 data = response.json()
+data_str = json.dumps(data, indent=4, ensure_ascii=False)
+print(data_str)
+print(f"Total de registros: {data_str.count('numero')}")
 
 print(f"Total de registros: {len(data)}")
